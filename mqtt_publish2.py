ff82770e01de72fa35487f93bc36c41acb1c6f6c
Ugyen <ugyen.c-epe2019@tggs.kmutnb.ac.th>
2021-01-10 20:16:11 +0100

    test

--- mqtt_publish2.py
+++ mqtt_publish2.py
@@ -17,7 +17,7 @@ while True:
     client.publish("Voltage/1", randNumber1)  # Voltage/1 is the Topic and randNumber1 is the payload/message
     client.publish("Voltage/2", randNumber2)
     client.publish("Voltage/3", randNumber3)
-    #client.publish("Voltage/4", randNumber4)
+    client.publish("Voltage/4", randNumber4)
 
     print("Published ", round(randNumber1, 2), " to Topic Voltage/1")
     print("Published ", round(randNumber2, 2), " to Topic Voltage/2")
